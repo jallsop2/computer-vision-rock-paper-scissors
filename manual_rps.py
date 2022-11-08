b09e81cff71e18323ef6b6dcf833bab7a11c3c71
Jonathan Allsop <jonathanallsop2@gmail.com>
2022-11-08 17:06:42 +0000

    Calculates who wins in manual rps

--- manual_rps.py
+++ manual_rps.py
@@ -1,23 +1,35 @@
 import random
 import sys
 
-choices = ['rock','paper','scissors']
 
-def get_computer_choice(choices):
-    computer_choice = random.choice(choices)
+def get_computer_choice():
+    computer_choice = random.choice(range(0,3))
     return computer_choice
 
-def get_user_choice(choices):
+def get_user_choice():
+
+    choices = ['rock','paper','scissors']
+
     while True:
         user_choice = input("\nRock Paper Scissors:")
 
         if type(user_choice) == 'str' and user_choice.lower() in choices:
-            return user_choice.lower()
+            return choices.index(user_choice.lower())
         
         elif user_choice == 'quit':
             sys.exit()
         
         else:
             print("\n Please input rock paper or scissors.")
+
+def get_winner(computer_choice,user_choice):
+    res = (user_choice - computer_choice)%3
+    if res == 1:
+        print('You won!')
+    elif res == 0:
+        print('It is a tie')
+    elif res == -1:
+        print('You lost')
+
             
 
