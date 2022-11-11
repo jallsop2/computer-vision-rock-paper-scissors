9a7b58f91fd66ca8973f2d8b224a13af6c972e79
Jonathan Allsop <jonathanallsop2@gmail.com>
2022-11-11 16:21:35 +0000

    Makes the game into a python class

--- camera_rps.py
+++ camera_rps.py
@@ -1,5 +1,4 @@
 import cv2
-import sys
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
@@ -17,109 +16,106 @@ model = load_model('keras_model.h5',compile = False)
 
 
 
-def get_computer_choice():
-    computer_choice = random.choice(range(0,3))
-    return computer_choice
+class rps():
+    def __init__(self):
+        self.user_score = 0
+        self.computer_score = 0
 
-def get_user_choice(switch):
+        self.user_choice = -2
+        self.computer_choice = -2
 
-    if switch == 1:
+            
+    def get_choices(self):
 
-        go = input('\nDo you want to continue? (y/n):  ')
+        self.computer_choice = random.choice(range(0,3))
+      
+        tic = time.time()
+        print('\n3')
+            
+        while True:
+            toc = time.time()
+            if toc - tic >= 1:
+                print('\n2')
+                break
+        while True:
+            toc = time.time()
+            if toc - tic >= 2:
+                print('\n1\n\n')
+                break
+        while True:
+            toc = time.time()
+            if toc - tic >= 3:
+                break
 
-        if go == 'n':
-            sys.exit()
-    
-    tic = time.time()
-    print('\n3')
+        cap = cv2.VideoCapture(0)
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        ret, frame = cap.read()
+        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+        data[0] = normalized_image
+        data_tf = convert(data,np.float32)
+        prediction = model.predict(data_tf)
+        #cv2.imshow('frame', frame)
+
+        self.user_choice = prediction.argmax()
         
-    while True:
-        toc = time.time()
-        if toc - tic >= 1:
-            print('\n2')
-            break
-    while True:
-        toc = time.time()
-        if toc - tic >= 2:
-            print('\n1\n\n')
-            break
-    while True:
-        toc = time.time()
-        if toc - tic >= 3:
-            break
+        cap.release()
+        # Destroy all the windows
+        cv2.destroyAllWindows()
 
-    cap = cv2.VideoCapture(0)
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    ret, frame = cap.read()
-    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-    image_np = np.array(resized_frame)
-    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-    data[0] = normalized_image
-    data_tf = convert(data,np.float32)
-    prediction = model.predict(data_tf)
-    #cv2.imshow('frame', frame)
-
-    choice = prediction.argmax()
-    
-    cap.release()
-    # Destroy all the windows
-    cv2.destroyAllWindows()
-    return choice
-
-def get_winner(computer_choice,user_choice):
+    def get_result(self):
 
 
-    if user_choice == 3:
-        print("\nYou didn't show anything!" )
-        return -1
-    
-    else:
-        choices = ['Rock','Paper','Scissors']
-        print(f'\nYou chose: {choices[user_choice]}')
-        print(f'\nThe computer chose: {choices[computer_choice]}')
+        if self.user_choice == 3:
+            print("\nYou didn't show anything!" )
+            return
+        
+        else:
+            choices = ['Rock','Paper','Scissors']
+            print(f'\nYou chose: {choices[self.user_choice]}')
+            print(f'\nThe computer chose: {choices[self.computer_choice]}')
 
-        res = (user_choice - computer_choice)%3
-        return res
-    
+            res = (self.user_choice - self.computer_choice)%3
+            
+            if res == 1:
+                print('\nYou get a point!')
+                self.user_score += 1
+            elif res == 0:
+                print('\nIt is a tie')
+            elif res == 2:
+                print('\nThe computer got a point!')
+                self.computer_score += 1
+            
+            print('\nThe score is:')
+            print(f'\n   Player {self.user_score} : {self.computer_score} Computer')
+
+            
+            
 
             
 def play():
 
-    switch = 0
-
-    user_score = 0
-    computer_score = 0
+    game = rps()
 
     while True:
 
-        computer_choice = get_computer_choice()
-
-        user_choice = get_user_choice(switch)
+        game.get_choices()
         
-        res = get_winner(computer_choice,user_choice)
-
-        switch = 1
-
-        if res == 1:
-            print('\nYou get a point!')
-            user_score += 1
-        elif res == 0:
-            print('\nIt is a tie')
-        elif res == 2:
-            print('\nThe computer got a point!')
-            computer_score += 1
-        
-        print('\n The score is:')
-        print(f'\n Player {user_score} : {computer_score} Computer')
-
-        if user_score == 3:
+        game.get_result()
+       
+        if game.user_score == 3:
             print('\nCongratulations, you won!')
             break
         
-        elif computer_score == 3:
+        elif game.computer_score == 3:
             print('\nSorry, the computer wins.')
             break
-        
+
+        go = input('\nDo you want to continue? (y/n):  ')
+
+        if go == 'n':
+            return
 
 play()
 
